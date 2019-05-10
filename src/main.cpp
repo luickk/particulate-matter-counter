a154cf9e0d38aadd5637ccbc36eb3933ac930873
luick klippel <gelberdev@gmail.com>
2019-05-11 00:49:04 +0200

    adding geiger counter like clicking

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,31 @@
 float p10, p25;
 int error;
 
+int sound_out=10;
+
+int randi = 100;
+
+int ranvol = 100;
+
 SDS011 my_sds;
 
 void setup() {
-	my_sds.begin(12, 11); //RX, TX
+	my_sds.begin(3, 2); //RX, TX
 	Serial.begin(9600);
-}
 
+	randomSeed(analogRead(0));
+	pinMode(sound_out, OUTPUT);
+	digitalWrite(sound_out, LOW);
+}
+int ranNumVol;
+int randNumber;
 void loop() {
+	ranNumVol = random (ranvol);
+	digitalWrite(sound_out, ranNumVol);
+	randNumber = random(randi);
+	delay(randNumber);
+	digitalWrite(sound_out, LOW);
+
 	error = my_sds.read(&p25, &p10);
 	if (!error) {
 		Serial.println("P2.5: " + String(p25));
